f65eafb490a6ecae93f9884989f52766bd60de64
Roberto Moreno <r.moreno@relay42.com>
2018-11-19 16:10:01 +0100

    Game1

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,51 +1,41 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var m_score1: Int = 0
-    private var m_score2: Int = 0
+    private val scores = mutableMapOf(player1Name to 0, player2Name to 0)
 
-    override fun wonPoint(playerName: String) {
-        if (playerName === "player1")
-            m_score1 += 1
-        else
-            m_score2 += 1
+    override fun wonPoint(playerName: String){
+        scores[playerName]?.let {
+            scores.plusAssign(playerName to (it + 1))
+        }
     }
 
     override fun getScore(): String {
-        var score = ""
-        var tempScore = 0
-        if (m_score1 == m_score2) {
-            when (m_score1) {
-                0 -> score = "Love-All"
-                1 -> score = "Fifteen-All"
-                2 -> score = "Thirty-All"
-                else -> score = "Deuce"
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
-            if (minusResult == 1)
-                score = "Advantage player1"
-            else if (minusResult == -1)
-                score = "Advantage player2"
-            else if (minusResult >= 2)
-                score = "Win for player1"
-            else
-                score = "Win for player2"
-        } else {
-            for (i in 1..2) {
-                if (i == 1)
-                    tempScore = m_score1
-                else {
-                    score += "-"
-                    tempScore = m_score2
-                }
-                when (tempScore) {
-                    0 -> score += "Love"
-                    1 -> score += "Fifteen"
-                    2 -> score += "Thirty"
-                    3 -> score += "Forty"
-                }
-            }
+        val player1Score = scores[player1Name]!!
+        val player2Score = scores[player2Name]!!
+        return when {
+            player1Score == player2Score -> drawToTennisScore(player1Score)
+            player1Score >= 4 || player2Score >= 4 -> deuceToTennisScore(player1Score - player2Score)
+            else -> pointsToTennisScore(player1Score) + "-" + pointsToTennisScore(player2Score)
         }
-        return score
     }
+}
+
+private fun pointsToTennisScore(score: Int): String = when (score) {
+    0 -> "Love"
+    1 -> "Fifteen"
+    2 -> "Thirty"
+    else -> "Forty"
+}
+
+private fun drawToTennisScore(points: Int): String = when (points) {
+    0 -> "Love-All"
+    1 -> "Fifteen-All"
+    2 -> "Thirty-All"
+    else -> "Deuce"
+}
+
+private fun deuceToTennisScore(minusResult: Int): String = when {
+    minusResult == 1 -> "Advantage player1"
+    minusResult == -1 -> "Advantage player2"
+    minusResult >= 2 -> "Win for player1"
+    else -> "Win for player2"
 }
